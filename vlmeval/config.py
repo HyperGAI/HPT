0041f76f9ebc4618b7390303c7f3bf9d3ca6421c
Size Wu (吴思泽) <72066971+wusize@users.noreply.github.com>
2024-05-01 01:17:38 +0800

    Update config.py

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -27,6 +27,7 @@ models = {
     'hpt-air-seed': partial(HPT, is_crop=False),
     'hpt-air-demo': partial(HPT, vis_scale=392, is_crop=False),
     'hpt-air-demo-local': partial(HPT, vis_scale=392, is_crop=False, global_model_path='../HPT_AIR_HF/'),
+    'hpt-air-pad': partial(HPT, pad_image_to_square=True),
 }
 
 supported_VLM = {}
